d82d0f5753559e6a56a1cdcb5e7c70297f77cf01
danick sikkema <danick.sikkema@gmail.com>
2015-12-11 14:18:33 +0100

    divide users in groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210131708) do
+ActiveRecord::Schema.define(version: 20151207133505) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "question"
@@ -28,14 +28,6 @@ ActiveRecord::Schema.define(version: 20151210131708) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string   "user_id"
-    t.integer  "time"
-    t.string   "collection_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
